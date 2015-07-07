ad9533930424fd3f7e311739441c1f3114d56875
Nicki Woodward <Nicola.woodward@bt.com>
2015-07-07 11:43:21 +0100

    totally awesome working file upload

--- app/controllers/homepage.rb
+++ app/controllers/homepage.rb
@@ -9,8 +9,15 @@ class TextConverter < Sinatra::Base
    erb :'homepage/index'
  end
  post "/clear_text/" do
-   @text_convert=''
+   @text_to_convert=''
    @converted_text=''
    erb :'homepage/index'
  end
-end
+ post "/upload_file/" do
+   @filename = params[:file][:filename]
+   file = params[:file][:tempfile]
+   @text_to_convert = file.read
+   @converted_text = Converter.convert_text(@text_to_convert)
+   erb :'homepage/index'
+ end
+ end

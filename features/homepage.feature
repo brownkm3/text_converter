9075c274fed1fd21a32db440c95966e8c04df515
Nicki Woodward <Nicola.woodward@bt.com>
2015-07-07 11:19:58 +0100

    failing features for file upload

--- features/homepage.feature
+++ features/homepage.feature
@@ -11,5 +11,10 @@ Feature: The Welcome Page
   Scenario: Clearing the text box
     When I go to the homepage
     And I submit text via the text box
-    And I click clear text 
-    Then I should get a clear text box 
+    And I click clear text
+    Then I should get a clear text box
+
+  Scenario: Converting Text
+    When I go to the homepage
+    And I upload a file to be converted
+    Then I should see the converted text
--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -6,7 +6,7 @@ Then "I should see the welcome page" do
       expect(page).to have_content("Hello Welcome to Text Converter")
 end
 
-Then "I submit text via the text box" do
+When "I submit text via the text box" do
   fill_in('Text to convert', with: "twas brillig and the slivy toves")
   click_on('Convert text')
 end
@@ -15,10 +15,16 @@ Then "I should see the converted text" do
   expect(page).to have_content("twas toves")
 end
 
-Then "I click clear text" do
+When "I click clear text" do
   click_on('Clear')
 end
 
 Then "I should get a clear text box" do
-  fill_in('Text to convert', with: "")
+  expect(page.body).to have_field('Text to convert', with: "")
+end
+
+
+When 'I upload a file to be converted' do
+  attach_file("file", File.join(Sinatra::Application.root, 'features', 'support', 'test.txt'))
+  click_button "Upload file"
 end
